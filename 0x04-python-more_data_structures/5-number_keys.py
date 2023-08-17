f164cb15f3a08debf18c95f46f83e7929c6d7b1a
Udeme Harrison <udeme-goc@gmail.com>
2023-08-17 08:49:13 +0100

    Update of 5-number_keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -14,9 +14,3 @@ def number_keys(a_dictionary):
     num_keys = len(a_dictionary)
 
     return num_keys
-
-# Test the function
-if __name__ == "__main__":
-    a_dictionary = {'language': "C", 'number': 13, 'track': "Low level"}
-    nb_keys = number_keys(a_dictionary)
-    print("Number of keys: {:d}".format(nb_keys))
