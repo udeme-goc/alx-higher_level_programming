41a9922feca60e848c5b8fe04bd7954504ce4e5e
Udeme Harrison <udeme-goc@gmail.com>
2023-08-29 23:14:15 +0100

    Indentation fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
-    """
-    Defines class square
-    """
+""" Defines class square """
 
 class Square:
-    """
-    This is the Square class documentation.
-    """
+    """ This is the Square class documentation. """
     pass
