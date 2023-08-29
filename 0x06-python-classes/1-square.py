eb99d256c91911422e93eedf7b668562f19e2291
Udeme Harrison <udeme-goc@gmail.com>
2023-08-29 23:54:16 +0100

    Modification of 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
+""" Define class Square. """
+
 class Square:
-    """
-    This is the Square class documentation.
-    """
-    def __init__(self, size):
-        """
-        Initializes a new Square instance.
-        Args:
-            size (int): The size of the square.
-        """
+    """ This is the Square class documentation. """
+    def __init__(self, size=0):
+        """ Initialize the square with a given size. """
         self.__size = size
+
+    def area(self):
+        """ Calculate and return the area of the square. """
+        return self.__self ** 2
