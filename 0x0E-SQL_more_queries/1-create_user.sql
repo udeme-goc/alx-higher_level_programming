0dbed038165a03cf1cfc38a01b5887e5cff21bc8
udeme-goc <udeme-goc@gmail.com>
2023-12-14 07:58:01 +0100

    SQL more queries initial upload

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Create user_0d_1, if it doesn't exist.
+-- Grant all privileges to user.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Create the table force_name, if not exists.
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT,
+	name VARCHAR(256) NOT NULL
+);
