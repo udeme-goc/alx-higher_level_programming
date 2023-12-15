9278661734cd9fa364598b45089a6ea22eb9ddf0
udeme-goc <udeme-goc@gmail.com>
2023-12-15 09:51:42 +0100

    SQL More Queries update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Create user_0d_1, if it doesn't exist.
 -- Grant all privileges to user.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO user_0d_1@localhost;
