1f4462278c8db477de9e0548ea94c0edd0673f4c
udeme-goc <udeme-goc@gmail.com>
2023-12-15 19:19:46 +0100

    update on More SQL Queries

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre0.sql
@@ -0,0 +1,8 @@
+-- Select genres and the number of shows linked to each.
+USE hbtn_0d_tvshows;
+SELECT tv_genres.name AS genre, COUNT(tv_shows.id) AS number_of_shows
+FROM tv_genres
+JOIN tv_shows_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC, genre ASC;
