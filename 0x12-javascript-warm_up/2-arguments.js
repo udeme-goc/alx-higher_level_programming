fcd41080a7e2a8081d386b9920228b89c0af95b6
udeme-goc <udeme-goc@gmail.com>
2024-01-10 02:32:35 +0100

    Review of some content

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,9 +5,9 @@ const numArgs = process.argv.length;
 
 // Check number of arguments and print corresponding message
 if (numArgs === 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (numArgs === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -5,7 +5,7 @@ const firstArg = process.argv[2];
 
 // Check if an argument is provided and print corresponding message
 if (typeof firstArg === 'undefined') {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	console.log(firstArg);
+  console.log(firstArg);
 }
