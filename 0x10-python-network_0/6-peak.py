bf4415102e13470f6245e9503d8c8b25c7729bc2
udeme-goc <udeme-goc@gmail.com>
2024-01-27 21:31:34 +0100

    PEP8 compliance for 6-peak.py:wq

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 This module contains a function to find a peak in a list of unsorted integers.
 """
 
+
 def find_peak(list_of_integers):
     """
     Find a peak in a list of unsorted integers.
@@ -34,6 +35,7 @@ def find_peak(list_of_integers):
 
     # Return the peak element
     return list_of_integers[low]
+
 
 # Test the function
 if __name__ == "__main__":
